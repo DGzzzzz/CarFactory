26d9ec92511ce6db8293899ff672c8480df3c605
DGzzzzz <douglaswinter@outlook.com>
2024-08-29 18:06:27 -0300

    :recycle: refactor: test

--- src/test/java/com/example/CarroTest.java
+++ src/test/java/com/example/CarroTest.java
@@ -2,13 +2,14 @@ package com.example;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-
 import com.example.domain.Carro;
+import com.example.domain.Marca;
 import com.example.dao.CarroDAO;
 
 public class CarroTest {
@@ -22,81 +23,95 @@ public class CarroTest {
 
     @After
     public void tearDown() {
-        carroDAO.delete(carroDAO.findById(1));
+        carroDAO.delete(carroDAO.findById(1L));
     }
     
     @Test
     public void testSave() {
+        Marca marca = new Marca();
+        marca.setNome("Fiat");
+
         Carro carro = new Carro();
-        carro.setMarca("Fiat");
+        carro.setCodigo("12345");
         carro.setModelo("Uno");
         carro.setAno(2010);
-        carro.setPreco(20000.0);
-        CarroDAO carroDAO = new CarroDAO();
+        carro.setMarca(marca);
         carroDAO.save(carro);
 
-        Carro savedCarro = carroDAO.findById(1);
+        Carro savedCarro = carroDAO.findById(1L);
         assertNotNull(savedCarro);
-        assertEquals("Fiat", savedCarro.getMarca());
+        assertEquals("12345", savedCarro.getCodigo());
         assertEquals("Uno", savedCarro.getModelo());
         assertEquals(2010, savedCarro.getAno());
-        assertEquals(20000.0, savedCarro.getPreco(), 0.0);
+        assertEquals("Fiat", savedCarro.getMarca().getNome());
     }
 
     @Test
     public void testUpdate() {
+        Marca marca = new Marca();
+        marca.setNome("Fiat");
+
         Carro carro = new Carro();
-        carro.setId(1);
-        carro.setMarca("Fiat");
+        carro.setId(1L);
+        carro.setCodigo("12345");
         carro.setModelo("Uno");
         carro.setAno(2010);
-        carro.setPreco(20000.0);
+        carro.setMarca(marca);
         carroDAO.save(carro);
 
-        carro.setMarca("Ford");
+        Marca novaMarca = new Marca();
+        novaMarca.setNome("Ford");
+
+        carro.setCodigo("67890");
         carro.setModelo("Ka");
         carro.setAno(2015);
-        carro.setPreco(30000.0);
+        carro.setMarca(novaMarca);
 
         carroDAO.update(carro);
 
-        Carro updatedCarro = carroDAO.findById(1);
+        Carro updatedCarro = carroDAO.findById(1L);
         assertNotNull(updatedCarro);
-        assertEquals("Ford", updatedCarro.getMarca());
+        assertEquals("67890", updatedCarro.getCodigo());
         assertEquals("Ka", updatedCarro.getModelo());
         assertEquals(2015, updatedCarro.getAno());
-        assertEquals(30000.0, updatedCarro.getPreco(), 0.0);
+        assertEquals("Ford", updatedCarro.getMarca().getNome());
     }
 
     @Test
     public void testDelete() {
+        Marca marca = new Marca();
+        marca.setNome("Fiat");
+
         Carro carro = new Carro();
-        carro.setMarca("Fiat");
+        carro.setCodigo("12345");
         carro.setModelo("Uno");
         carro.setAno(2010);
-        carro.setPreco(20000.0);
+        carro.setMarca(marca);
         carroDAO.save(carro);
 
         carroDAO.delete(carro);
 
-        Carro deletedCarro = carroDAO.findById(1);
+        Carro deletedCarro = carroDAO.findById(1L);
         assertNull(deletedCarro);
     }
 
     @Test
     public void testFindById() {
+        Marca marca = new Marca();
+        marca.setNome("Fiat");
+
         Carro carro = new Carro();
-        carro.setMarca("Fiat");
+        carro.setCodigo("12345");
         carro.setModelo("Uno");
         carro.setAno(2010);
-        carro.setPreco(20000.0);
+        carro.setMarca(marca);
         carroDAO.save(carro);
 
-        Carro foundCarro = carroDAO.findById(1);
+        Carro foundCarro = carroDAO.findById(1L);
         assertNotNull(foundCarro);
-        assertEquals("Fiat", foundCarro.getMarca());
+        assertEquals("12345", foundCarro.getCodigo());
         assertEquals("Uno", foundCarro.getModelo());
         assertEquals(2010, foundCarro.getAno());
-        assertEquals(20000.0, foundCarro.getPreco(), 0.0);
+        assertEquals("Fiat", foundCarro.getMarca().getNome());
     }
 }
