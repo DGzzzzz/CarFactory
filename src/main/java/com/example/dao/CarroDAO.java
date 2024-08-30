5e7bd7a875676da913c370332433c06ad94d6720
DGzzzzz <douglaswinter@outlook.com>
2024-08-30 10:24:21 -0300

    :sparkles: feat: add criteria api in method findByID

--- src/main/java/com/example/dao/CarroDAO.java
+++ src/main/java/com/example/dao/CarroDAO.java
@@ -1,5 +1,10 @@
 package com.example.dao;
 
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -36,7 +41,12 @@ public class CarroDAO implements ICarroDAO {
     @Override
     public Carro findById(Long id) {
         Session session = HibernateUtil.getSessionFactory().openSession();
-        Carro carro = session.get(Carro.class, id);
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<Carro> cq = cb.createQuery(Carro.class);
+        Root<Carro> root = cq.from(Carro.class);
+        cq.select(root).where(cb.equal(root.get("id"), id));
+        TypedQuery<Carro> query = session.createQuery(cq);
+        Carro carro = query.getSingleResult();
         session.close();
         return carro;
     }
